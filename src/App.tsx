ca738447cb9ed171072709a6569dbe024d18dc0d
Justin Gruenenwald <Justin.Gruenenwald@faro.com>
2023-05-09 23:17:35 +0900

    frontend can fetch and display image from backend

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,36 @@ import React, { useState, useEffect } from 'react';
 
 const App: React.FC = () => {
   const [images, setImages] = useState<string[]>([]);
-
+  const [fetchedImage, setFetchedImage] = useState<string | null>(null);
+  const [filename, setFilename] = useState<string>('default_x.jpg');
   useEffect(() => {
     fetchImages();
   }, []);
 
   const fetchImages = async () => {
-    const response = await fetch("http://localhost:8000/test")
-    const todos = await response.json()
+    const response = await fetch("http://localhost:8000/images")
+    const image_ids = await response.json()
+    for (let i = 0; i < image_ids.length; i++) {
+      const image_url = `http://localhost:8000/images/${image_ids[i].toString()}`
+      const image = await fetch(image_url)
+      const blob = await image.blob()
+      const fetchedImageUrl = URL.createObjectURL(blob);
+      setFetchedImage(fetchedImageUrl);
+
+      const contentDisposition = response.headers.get("Content-Disposition");
+      const filenameTmp = contentDisposition ? contentDisposition.split(";")[1].split("=")[1] : "default.jpg";
+      setFilename(filenameTmp)
+    }
+
   };
 
 
   return (
     <div>
       <h1>Images</h1>
-      {images.map((image, index) => (
-        <img key={index} src={image} alt={`Image ${index}`} style={{ width: '300px', height: 'auto' }} />
-      ))}
+      {fetchedImage && (
+        <img src={fetchedImage} alt={filename} style={{ maxWidth: '100%', marginTop: '10px' }} />
+      )}
     </div>
   );
 };
