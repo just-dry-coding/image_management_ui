bbf93dc47147716d3dd11e5721a749ab46137d2d
Justin Gruenenwald <Justin.Gruenenwald@faro.com>
2023-05-14 14:47:42 +0900

    finish up frontend with dynamic image grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,87 @@
 import React, { useState, useEffect } from 'react';
+import { ImageGallery, Image } from './components/ImageGallery';
+import axios from 'axios';
 
 const App: React.FC = () => {
-  const [images, setImages] = useState<string[]>([]);
-  const [fetchedImage, setFetchedImage] = useState<string | null>(null);
-  const [filename, setFilename] = useState<string>('default_x.jpg');
+  const [images, setImages] = useState<Image[]>([]);
+
   useEffect(() => {
     fetchImages();
   }, []);
 
+  const fetchImage = async (imageId: string) => {
+    const response = await fetch(`http://localhost:8000/images/${imageId}`)
+    const blob = await response.blob()
+    const localUrl = URL.createObjectURL(blob);
+
+    const contentDisposition = response.headers.get("Content-Disposition");
+    const filename = contentDisposition ? contentDisposition.split(";")[1].split("=")[1] : "default.jpg";
+    console.log(filename)
+    return new Image(filename.replace(/"/g, ''), localUrl, imageId)
+  }
+
   const fetchImages = async () => {
     const response = await fetch("http://localhost:8000/images")
     const image_ids = await response.json()
+
+    let imagesLocal: Image[] = []
     for (let i = 0; i < image_ids.length; i++) {
-      const image_url = `http://localhost:8000/images/${image_ids[i].toString()}`
-      const image = await fetch(image_url)
-      const blob = await image.blob()
-      const fetchedImageUrl = URL.createObjectURL(blob);
-      setFetchedImage(fetchedImageUrl);
-
-      const contentDisposition = response.headers.get("Content-Disposition");
-      const filenameTmp = contentDisposition ? contentDisposition.split(";")[1].split("=")[1] : "default.jpg";
-      setFilename(filenameTmp)
+      const image = await fetchImage(image_ids[i].toString())
+      imagesLocal.push(image)
     }
-
+    setImages(imagesLocal)
   };
 
+  const onEdit = (index: number) => {
+    try {
+      const fileInput = document.createElement('input');
+      fileInput.type = 'file';
+      fileInput.accept = 'image/*';
+      fileInput.click();
+
+      fileInput.onchange = async (e: any) => {
+        const uploadedImage = e.target.files[0]
+        const url = URL.createObjectURL(uploadedImage);
+        images[index].name = uploadedImage.name
+        images[index].url = url
+
+        const formData = new FormData()
+        formData.append('file', uploadedImage)
+
+        try {
+          await axios.put(`http://localhost:8000/images/${images[index].id}`, formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          });
+
+          console.log('Image edited successfully');
+        } catch (error) {
+          console.error('Error editing image', error);
+        }
+      };
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
+  }
+
+  const onDelete = async (index: number) => {
+    try {
+      await axios.delete(`http://localhost:8000/images/${images[index].id}`);
+
+      images.splice(index, 1)
+      setImages(images)
+
+      console.log('Image deleted successfully');
+    } catch (error) {
+      console.error('Error deleting image', error);
+    }
+  };
 
   return (
     <div>
-      <h1>Images</h1>
-      {fetchedImage && (
-        <img src={fetchedImage} alt={filename} style={{ maxWidth: '100%', marginTop: '10px' }} />
-      )}
+      <h2>MQTT image management UI</h2>
+      <ImageGallery images={images} onEdit={onEdit} onDelete={onDelete} />
     </div>
   );
 };
---
+++ src/components/ImageGallery.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import Masonry from 'react-masonry-css';
+import './styles/ImageGallery.css'
+
+export class Image {
+    name: string;
+    url: string;
+    id: string
+
+    constructor(name: string, url: string, id: string) {
+        this.name = name;
+        this.url = url;
+        this.id = id;
+    }
+}
+
+interface Props {
+    images: Image[]
+    onEdit: (index: number) => void
+    onDelete: (index: number) => void
+}
+
+export const ImageGallery = ({ images, onEdit, onDelete }: Props) => {
+
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+    const handleMouseEnter = (index: number) => {
+        setHoveredIndex(index);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredIndex(null);
+    };
+
+    const breakpointColumns = {
+        default: 3,
+        1100: 2,
+        700: 1,
+    };
+
+    return (
+        <div style={{
+            display: 'flex'
+        }}>
+            <Masonry
+                breakpointCols={breakpointColumns}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+            >
+                {images.map((image, index) => (
+                    <div className="image-wrapper" key={index}
+                        onMouseEnter={() => handleMouseEnter(index)}
+                        onMouseLeave={handleMouseLeave}>
+                        <img className="image-item" src={image.url} alt={image.name} />
+                        <div className="image-text" >{image.name}</div>
+                        {hoveredIndex === index && (
+                            <div className="button-container">
+                                <button className="edit-button" onClick={() => onEdit(index)}>Edit</button>
+                                <button className="delete-button" onClick={() => onDelete(index)}>Delete</button>
+                            </div>
+                        )}
+                    </div>
+                ))}
+
+            </Masonry>
+        </div >
+    );
+};
